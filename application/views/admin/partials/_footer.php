277478152fb0616f377447994a01683f69cdf4f5
mdestafadilah <desta.08b@gmail.com>
2022-03-21 05:55:40 +0700

    MOCK LAYOUT - Status Draft - Experimental

---
+++ application/views/admin/partials/_footer.php
@@ -0,0 +1,18 @@
+
+
+<!-- Main Footer -->
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+        Bantuan Hukum
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2022 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+</footer>
+</div><!-- ./wrapper -->
+
+<script src="<?php echo base_url('assets/admin'); ?>/plugins/jquery/jquery.min.js"></script>
+<script src="<?php echo base_url('assets/admin'); ?>/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?php echo base_url('assets/admin'); ?>/js/adminlte.min.js"></script>
+</body>
+</html>
