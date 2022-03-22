c90286c4d168dc8ec5c131eede750763adb84c7c
rizskydarmawann <70370415+rizskydarmawann@users.noreply.github.com>
2022-03-22 10:52:01 +0700

    test
    test

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://bantuanhukum.local/';
+$config['base_url'] = 'http://localhost/bantuanhukum/';
 
 /*
 |--------------------------------------------------------------------------
